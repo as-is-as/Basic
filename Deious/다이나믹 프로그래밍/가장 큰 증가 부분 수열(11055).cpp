9686139629d4c9f3eb7580999d0ef713e1e85c91
deious <gkstjddlekd@naver.com>
2020-01-01 22:32:45 +0900

    Solve DP problem #2

---
+++ Deious/다이나믹 프로그래밍/가장 큰 증가 부분 수열(11055).cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+int check[1001] = {0,};
+int num[1001];
+
+int main()
+{
+	int a, answer = 0;
+	scanf("%d", &a);
+
+	for (int i = 1; i <= a; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (num[j] < num[i] && check[i] < check[j] + num[i])
+			{
+				check[i] = check[j] + num[i];
+			}
+		}
+	}
+
+	for (int i = 1; i <= 1000; i++)
+	{
+		if (check[i] > answer)
+		{
+			answer = check[i];
+		}
+	}
+	printf("%d", answer);
+
+	return 0;
+}
